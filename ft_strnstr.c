b07889955fe31ec73bc92117860f5e51b9cf1683
Kooneo <husa.bakour02@gmail.com>
2024-10-24 13:12:09 +0100

    strnstr and atoi

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,6 +1,29 @@
 #include "libft.h"
 
-char ft_strnstr(const char *big, const char *little, size_t len)
+char *ft_strnstr(const char *big, const char *little, size_t len)
 {
+    size_t i;
+    size_t j;
+    const char *p_sbig;
     
+    if (*little == '\0')
+        return ((char *)big);
+    i = 0;
+    while (i < len && big[i] != '\0')
+    {
+        j = 0;
+        if (big[i] == little[j])
+        {
+            p_sbig = &big[i];
+            while( (i + j) < len &&big[i + j] == little[j] && little[j] != '\0')
+            {
+                j++;   
+            }
+            if ( little[j] == '\0')
+                return ((char *)p_sbig);
+                
+        }
+        i++;
+    }
+    return (NULL);
 }
--- main.c
+++ main.c
@@ -1,39 +1,13 @@
 #include "libft.h"
 #include <stdio.h>
 #include <string.h>
+#include	<stdlib.h>
 
 int main()
 {
-    char dest1[20] = "Hello ";
-    char dest2[20] = "Hello ";
-    char dest3[20] = "Hello ";
-    char dest4[5] = "Hi";
-    const char *src = "World!";
-    size_t result;
-
-    // Test case 1: Normal concatenation
-    result = ft_strlcat(dest1, src, sizeof(dest1));
-    printf("Test 1:\n");
-    printf("Result: %zu\n", result);
-    printf("Destination: %s\n\n", dest1);
-
-    // Test case 2: Truncation
-    result = ft_strlcat(dest2, src, 10);
-    printf("Test 2:\n");
-    printf("Result: %zu\n", result);
-    printf("Destination: %s\n\n", dest2);
-
-    // Test case 3: No space for concatenation
-    result = ft_strlcat(dest3, src, 6);
-    printf("Test 3:\n");
-    printf("Result: %zu\n", result);
-    printf("Destination: %s\n\n", dest3);
-
-    // Test case 4: Destination buffer too small
-    result = ft_strlcat(dest4, src, sizeof(dest4));
-    printf("Test 4:\n");
-    printf("Result: %zu\n", result);
-    printf("Destination: %s\n\n", dest4);
-
+    char str[] = "--56";
+    printf("%d\n", ft_atoi(str));
+    printf("%d", atoi(str));
+    // printf("%d", 325 / 10);
     return 0;
 }
